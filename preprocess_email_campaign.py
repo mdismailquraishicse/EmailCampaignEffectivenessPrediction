81681f05da7c5b3f6c9fd2d87409f6bb25209454
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2024-08-05 13:02:20 +0530

    get_vif method defined

--- preprocess_email_campaign.py
+++ preprocess_email_campaign.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 class PreprocessEmailCompaign:
     def __init__(self, df):
         self.df = df
+        self.features = list(df.columns)
     def nullHandler(self):
         drop_cols = ['Customer_Location'] # drop the columns because already lost important informations.
         impute_cols = ['Total_Links', 'Total_Images', 'Total_Past_Communications'] # impute these columns with median or mean
@@ -14,5 +16,11 @@ class PreprocessEmailCompaign:
             median = self.df[col].quantile(.5)
             self.df[col] = self.df[col].fillna(median)
 
-    def eda(self):
-        pass
+    def get_vif(self, excluded):
+        features =[i for i in self.df.columns if i not in excluded]
+        vif = {features[i]:float(variance_inflation_factor(self.df[features].values,i)) for i in range(len(features))}
+        vif = pd.Series(vif)
+        features = list(vif.index)
+        features.append('Email_Status')
+        self.features = features
+        return vif
